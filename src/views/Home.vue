1c3d70cf727e4136f0d2bf97f5287c5327f49a97
William Caouette <william.caouette2001@gmail.com>
2021-10-14 09:21:55 -0400

    installation sur mon poste et ajout d'élément dans la base de donnée

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,17 +18,62 @@ export default {
         {
           id:0,
           name: "lire",
-          description : "blablalblal",
+          description : "exemple d'utilisation de l'application",
+          deletionDate : null,
+          elements:[
+            {
+              title: "livre 1",
+              isCompleted: false
+            },
+            {
+              title: "livre 2",
+              isCompleted: false
+            },
+            {
+              title: "livre 3",
+              isCompleted: false
+            }
+          ]
         },
         {
           id:1,
-          name: "lire",
-          description : "blablalblal",
+          name: "tâches",
+          description : "exemple d'utilisation de l'application",
+          deletionDate : null,
+          elements:[
+            {
+              title: "lessive",
+              isCompleted: false
+            },
+            {
+              title: "vaissel",
+              isCompleted: false
+            },
+            {
+              title: "cuisine",
+              isCompleted: false
+            }
+          ]
         },
         {
           id:2,
-          name: "lire",
-          description : "blablalblal",
+          name: "épicerie",
+          description : "exemple d'utilisation de l'application",
+          deletionDate : null,
+          elements:[
+            {
+              title: "Lait",
+              isCompleted: false
+            },
+            {
+              title: "Pain",
+              isCompleted: false
+            },
+            {
+              title: "Beurre d'arachide",
+              isCompleted: false
+            }
+          ]
         },
       ]
 
