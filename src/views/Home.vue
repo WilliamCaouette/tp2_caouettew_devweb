7ccd5a2c682218d211d8741d067ae49bc2e137bb
William Caouette <william.caouette2001@gmail.com>
2021-10-18 19:21:47 -0400

    home page quasiement fini

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="home">
     <left-nav-bar :lists="lists"></left-nav-bar>
-    
+    <div class="list-lists">
+        <div v-for="list in nonDeletedLists" :key="list.id" class="list">
+          <h3>{{list.name}}</h3>
+          <p>{{list.description}}</p>
+          <button @click="deleteList(list)">delete</button>
+          <ul>
+            <li v-for="element in list.elements" :key="element.id">
+              <input v-if="element.isCompleted" checked type="checkbox" name="isCompleted" >
+              <input v-else type="checkbox" name="isCompleted" >
+              {{element.title}}
+            </li>
+          </ul>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -13,7 +26,22 @@ export default {
   components: {
     LeftNavBar
   },
-  props:["lists"],
+  methods:{
+    deleteList(list){
+      list.deletionDate = this.currentTime;
+    }
+  },
+  computed:{
+    nonDeletedLists(){
+      return this.lists.filter((list)=>{
+        return list.deletionDate == null;
+      });
+    },
+    currentTime(){
+      let now = new Date();
+      return now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
+    },
+  },
   data(){ 
     return {
       lists : [
@@ -24,14 +52,17 @@ export default {
           deletionDate : null,
           elements:[
             {
+              id:0,
               title: "livre 1",
               isCompleted: false
             },
             {
+              id:1,
               title: "livre 2",
               isCompleted: false
             },
             {
+              id:2,
               title: "livre 3",
               isCompleted: false
             }
@@ -44,14 +75,16 @@ export default {
           deletionDate : null,
           elements:[
             {
+              id:0,
               title: "lessive",
               isCompleted: false
             },
             {
+              id:1,
               title: "vaissel",
               isCompleted: false
             },
-            {
+            {id:2,
               title: "cuisine",
               isCompleted: false
             }
@@ -64,14 +97,17 @@ export default {
           deletionDate : null,
           elements:[
             {
+              id:0,
               title: "Lait",
               isCompleted: false
             },
             {
+              id:1,
               title: "Pain",
               isCompleted: false
             },
             {
+              id:2,
               title: "Beurre d'arachide",
               isCompleted: false
             }
