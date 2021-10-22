245c53e775146ff59394d1b40a37cb65355ee4be
William Caouette <william.caouette2001@gmail.com>
2021-10-22 07:25:37 -0400

    ajouts du text field pour ajouter des éléments ainsi que des méthodes qui l'accompagne

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,16 @@
         <div v-for="list in nonDeletedLists" :key="list.id" class="list">
           <h3>{{list.name}}</h3>
           <p>{{list.description}}</p>
+          <input 
+            type="text" name="element" :data-pos="list.id" placeholder="Ajouter un élément à votre liste"
+            @keypress.enter="addElementToList"
+          >
           <button @click="deleteList(list)">delete</button>
           <ul>
-            <li v-for="element in list.elements" :class="element.isCompleted ? 'complete' :  ''" :key="element.id">
+            <li
+             v-for="element in list.elements" @click="changeElementStatus(element)"
+             :class="element.isCompleted ? 'complete' :  ''" :key="element.id"
+             >
               <input v-if="element.isCompleted" checked type="checkbox" name="isCompleted" >
               <input v-else type="checkbox" name="isCompleted" >
               {{element.title}}
@@ -20,6 +27,7 @@
 <style scoped>
   li{
     list-style: none;
+    cursor: pointer;
   }
   .complete{
     text-decoration: line-through;
@@ -37,6 +45,19 @@ export default {
   methods:{
     deleteList(list){
       list.deletionDate = this.currentDate;
+    },
+    changeElementStatus(element){
+      element.isCompleted = !element.isCompleted;
+    },
+    addElementToList(e){
+      console.log(e.target.value)
+      this.lists[e.target.dataset.pos].elements.push(
+        {
+          id: this.lists[e.target.dataset.pos].elements.length,
+          title: e.target.value,
+          isCompleted:false
+        });
+        e.target.value = "";
     }
   },
   computed:{
