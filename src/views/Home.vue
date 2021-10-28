1146825c2a11a9c9e9bb6751bcbf61be69c9e111
William Caouette <william.caouette2001@gmail.com>
2021-10-28 18:12:15 -0400

    ajout des commentaires présentant les fonctions dans le Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,17 +46,39 @@ export default {
     }
   },
   methods:{
+
+    /**
+     * @author William Caouette
+     * @description change la valeur du filter et est appeler via le left nav bar
+     */
     changeFilter(value){
       this.filter = value;
     },
+
+    /**
+     * @author William Caouette
+     * @description sauvegarde les données dans le local Storage
+     */
     saveDatasInLocalStorage(){
       let stringnifyLists = JSON.stringify(this.lists);
       localStorage.setItem("savedList", stringnifyLists);
     },
+
+    /**
+     * @author William Caouette
+     * @description Supprime une liste est appeler via des évènements
+     * @param e : evenement du clique sur bouton Supprimer
+     */
     deleteList(e){
       this.lists[e.target.dataset.pos].deletionDate = this.currentDate;
       this.saveDatasInLocalStorage();
     },
+
+    /**
+     * @author William Caouette
+     * @description ajoute un élément à la liste souhaiter
+     * @param e : évènement du clique sur le bouton qui contient l'ID de la liste dans un Dataset
+     */
     addElementToList(e){
       if(e.target.value != ""){
         this.lists[e.target.dataset.pos].elements.push(
@@ -70,6 +92,12 @@ export default {
       this.saveDatasInLocalStorage();
       }
     },
+
+    /**
+     * @author William Caouette
+     * @description ajoute une liste vide
+     * @param theme : thème de la liste que l'utilisateur à choisie (détermine l'image)
+     */
     addList(theme){
       this.lists.push(
         {
@@ -86,15 +114,33 @@ export default {
     },
   },
   computed:{
+
+    /**
+     * @author William Caouette
+     * @description filtre les liste pour présenter les listes qui ne sont pas supprimer
+     * @returns la liste filtrer
+     */
     nonDeletedLists(){
       return this.lists.filter((list)=>{
         return list.deletionDate == null;
       });
     },
+
+    /**
+     * @author William Caouette
+     * @description permet d'obtenire la date actuelle
+     * @returns retourne une chaine de caractère présentant la date actuelle sous forme de YYYY-MM-DD
+     */
     currentDate(){
       let now = new Date();
       return now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
     },
+
+    /**
+     * @author William Caouette
+     * @description applique le filtre de recherche entrée par l'utilisateur
+     * @returns la liste filtrer selons les lettre entrée dans le champs texte
+     */
     filteredList(){
       return this.lists.filter((list)=>{
         return list.name.includes(this.filter);
